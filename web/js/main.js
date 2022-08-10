212a14c0e0d1e6aabf5fe2517bceece8c63b88a4
Linden <65407488+thelindat@users.noreply.github.com>
2022-08-11 05:30:11 +1000

    feat(web/js): set eye colour when target changes

--- web/js/main.js
+++ web/js/main.js
@@ -1,13 +1,25 @@
 import { createOptions } from './createOptions.js';
 
+const optionsWrapper = document.getElementById('options-wrapper');
+const body = document.body;
+const eye = document.getElementById('eye');
+
 window.addEventListener('message', (event) => {
-  document.getElementById('options-wrapper').innerHTML = '';
+  optionsWrapper.innerHTML = '';
 
   switch (event.data.event) {
-    case 'visible':
-      document.body.style.visibility = event.data.state ? 'visible' : 'hidden';
+    case 'visible': {
+      body.style.visibility = event.data.state ? 'visible' : 'hidden';
+      return (eye.style.color = 'black');
+    }
 
-    case 'setTarget':
+    case 'leftTarget': {
+      return (eye.style.color = 'black');
+    }
+
+    case 'setTarget': {
+      eye.style.color = '#fdfdfd';
+
       if (event.data.options) {
         for (const type in event.data.options) {
           event.data.options[type].forEach((data, id) => {
@@ -15,5 +27,6 @@ window.addEventListener('message', (event) => {
           });
         }
       }
+    }
   }
 });
