cd50e63398b22867ebb28cb4ec59e673626e5279
Linden <65407488+thelindat@users.noreply.github.com>
2022-08-11 00:21:00 +1000

    feat(client): setup commands and keybind

--- client/main.lua
+++ client/main.lua
@@ -1,10 +1,12 @@
 local RaycastFromCamera = RaycastFromCamera
+local isActive = false
 
-CreateThread(function()
+local function enableTargetting()
+    isActive = true
     local drawSprites = DrawSprites and DrawSprites()
     local lastEntity
 
-    while true do
+    while isActive do
         local entityHit, endCoords, surfaceNormal, materialHash = RaycastFromCamera()
 
         for i = 1, 20 do
@@ -32,4 +34,28 @@ CreateThread(function()
             if i ~= 20 then Wait(0) end
         end
     end
-end)
+
+    if lastEntity then
+        SetEntityDrawOutline(lastEntity, false)
+    end
+end
+
+---@param forceDisable boolean
+local function disableTargetting(forceDisable)
+    isActive = false
+end
+
+-- Toggle ox_target, instead of holding the hotkey
+local toggleHotkey = GetConvarInt('ox_target:toggleHotkey', 0) == 1
+
+-- Default keybind to toggle targetting (https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard)
+local hotkey = GetConvar('ox_target:defaultHotkey', 'LMENU')
+
+if toggleHotkey then
+    RegisterCommand('ox_target', function() return isActive and disableTargetting() or enableTargetting() end)
+	RegisterKeyMapping("ox_target", "Toggle targetting", "keyboard", hotkey)
+else
+    RegisterCommand('+ox_target', function() CreateThread(enableTargetting) end)
+    RegisterCommand('-ox_target', disableTargetting)
+    RegisterKeyMapping('+ox_target', 'Toggle targetting', 'keyboard', hotkey)
+end
