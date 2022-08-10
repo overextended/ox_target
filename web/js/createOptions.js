51e0f1380ccca89ce1f6c3ff49d91ebaac8a00a8
Luke <sabolukas03@gmail.com>
2022-08-10 20:09:58 +0200

    feat(web): improve styling

--- web/js/createOptions.js
+++ web/js/createOptions.js
@@ -1,4 +1,4 @@
-import { fetchNui } from "./fetchNui.js";
+import { fetchNui } from './fetchNui.js';
 
 <<<<<<< refs/remotes/origin/main
 function selectOption(type, id) {
@@ -17,21 +17,27 @@ export function createOptions(type, data, id) {
   document.getElementById("menu").appendChild(child);
 =======
 function selectOption(target, id) {
-  fetchNui("select", { target: target, option: id });
-  document.body.style.visibility = "hidden";
+  fetchNui('select', { target: target, option: id });
+  document.body.style.visibility = 'hidden';
 }
+
+const optionsWrapper = document.getElementById('options-wrapper');
 
 export function createOptions(data, target) {
   if (data) {
-    const child = document.createElement("p");
-    child.className = "child";
-    child.innerHTML = `<i class="${
-      data.icon || "fa-solid fa-circle-info"
-    }"></i> ${data.label}`;
-    child.addEventListener("click", () => {
+    // option.innerHTML = `<i class="${
+    //   data.icon || "fa-solid fa-circle-info"
+    // }"></i> ${data.label}`;
+    const option = document.createElement('div');
+    option.className = 'option-container';
+    option.innerHTML = `
+      <i class="${data.icon} option-icon"></i>
+      <p class="option-label">${data.label}</p>
+    `;
+    option.addEventListener('click', () => {
       selectOption(target, id + 1);
     });
-    document.body.appendChild(child);
+    optionsWrapper.appendChild(option);
   }
 >>>>>>> test(web): horrible vanilla js
 }
--- web/js/main.js
+++ web/js/main.js
@@ -1,19 +1,16 @@
-import { createOptions } from "./createOptions.js";
+import { createOptions } from './createOptions.js';
 
-window.addEventListener("message", (event) => {
-<<<<<<< refs/remotes/origin/main
-  document.getElementById("menu").innerHTML = "";
-=======
-  document.querySelectorAll(".child").forEach((child) => {
-    child.remove();
+window.addEventListener('message', (event) => {
+  document.querySelectorAll('.option-container').forEach((option) => {
+    option.remove();
   });
 >>>>>>> test(web): horrible vanilla js
 
   switch (event.data.event) {
-    case "visible":
-      document.body.style.visibility = event.data.state ? "visible" : "hidden";
+    case 'visible':
+      document.body.style.visibility = event.data.state ? 'visible' : 'hidden';
 
-    case "setTarget":
+    case 'setTarget':
       if (event.data.options) {
         for (const type in event.data.options) {
 <<<<<<< refs/remotes/origin/main
