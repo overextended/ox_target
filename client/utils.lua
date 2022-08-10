728397bc636710abf2e2aa40ab5d2a968e1c1351
Linden <65407488+thelindat@users.noreply.github.com>
2022-08-11 05:53:11 +1000

    perf(client/utils): don't check zone:contains each frame

--- client/utils.lua
+++ client/utils.lua
@@ -48,7 +48,8 @@ if GetConvarInt('ox_target:drawSprite', 1) == 1 then
 
                 for _, zone in pairs(Zones) do
                     if zone.drawSprite ~= false and zone.distance < 7 then
-                        zone.colour = zone:contains(coords) and hover or colour
+                        local contains = zone:contains(coords)
+                        zone.colour = contains and hover or colour
                         n += 1
                         inRange[n] = zone
                     end
@@ -60,11 +61,9 @@ if GetConvarInt('ox_target:drawSprite', 1) == 1 then
             for i = 1, #inRange do
                 local zone = inRange[i]
 
-                if zone.drawSprite ~= false and zone.distance < 7 then
-                    local drawSprite = zone:contains(coords) and hover or colour
-
+                if zone.drawSprite ~= false then
                     SetDrawOrigin(zone.coords.x, zone.coords.y, zone.coords.z)
-                    DrawSprite(dict, texture, 0, 0, width, height, 0, drawSprite[1], drawSprite[2], drawSprite[3], drawSprite[4])
+                    DrawSprite(dict, texture, 0, 0, width, height, 0, zone.colour[1], zone.colour[2], zone.colour[3], zone.colour[4])
                     ClearDrawOrigin()
                 end
             end
