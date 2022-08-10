65dd18e8f9486c7ffc4d069fd888d4d4e006bd5f
Linden <65407488+thelindat@users.noreply.github.com>
2022-08-11 04:15:52 +1000

    feat(client): nui messages
    Setup messages to toggle visibility, set focus and controls, and handle
    select nui callback.

--- client/main.lua
+++ client/main.lua
@@ -11,11 +11,11 @@ end)
 
 local hasFocus = false
 
-local function setNuiFocus(state)
+local function setNuiFocus(state, cursor)
     if state then SetCursorLocation(0.5, 0.5) end
 
     hasFocus = state
-    SetNuiFocus(state, state)
+    SetNuiFocus(state, cursor or false)
     SetNuiFocusKeepInput(state)
 end
 
@@ -25,14 +25,17 @@ local GetEntityType = GetEntityType
 local GetEntityModel = GetEntityModel
 local GetEntityOptions = GetEntityOptions
 local SendNUIMessage = SendNUIMessage
+local IsDisabledControlJustPressed = IsDisabledControlJustPressed
+local DisableControlAction = DisableControlAction
+local options
 
 local function enableTargeting()
     if isDisabled or isActive or IsNuiFocused() then return end
+    SendNUIMessage({ event = 'visible', state = true })
 
     isActive = true
     local getNearbyZones, drawSprites = DrawSprites()
     local nearbyZones, lastEntity, entityType, entityModel
-    local options
 
     while isActive do
         local entityHit, endCoords, surfaceNormal, materialHash = RaycastFromCamera()
@@ -84,6 +87,17 @@ local function enableTargeting()
                 drawSprites(endCoords)
             end
 
+            if options and not hasFocus and IsDisabledControlJustPressed(0, 25) then
+                setNuiFocus(true, true)
+            end
+
+            if hasFocus then
+                DisableControlAction(0, 1, true)
+                DisableControlAction(0, 2, true)
+            end
+
+            DisablePlayerFiring(cache.playerId, true)
+
             if i ~= 20 then Wait(0) end
         end
     end
@@ -91,6 +105,10 @@ local function enableTargeting()
     if lastEntity then
         SetEntityDrawOutline(lastEntity, false)
     end
+
+    setNuiFocus(false)
+    SendNUIMessage({ event = 'visible', state = false })
+    options = nil
 end
 
 ---@param forceDisable boolean
@@ -112,3 +130,20 @@ else
     RegisterCommand('-ox_target', disableTargeting)
     RegisterKeyMapping('+ox_target', 'Toggle targeting', 'keyboard', hotkey)
 end
+
+RegisterNUICallback('select', function(data, cb)
+    cb(1)
+    local selection = options?[data[1]][data[2]]
+    isActive = false
+
+    if selection then
+        if selection.export then
+            local resource, method = string.strsplit('.', selection.export)
+            exports[resource](nil, method, selection)
+        end
+
+        if selection.event then
+            TriggerEvent(selection.event, selection)
+        end
+    end
+end)
