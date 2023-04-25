6455fda63793990ef0e3927fe4b501e8559acb50
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-04-25 00:21:13 -0400

    refactor(web): experimental addition of modifiable icon
    Totally untested

--- web/js/main.js
+++ web/js/main.js
@@ -3,22 +3,38 @@ import { createOptions } from './createOptions.js';
 const optionsWrapper = document.getElementById('options-wrapper');
 const body = document.body;
 const eye = document.getElementById('eyeSvg');
+const defaultIcon = "fa-solid fa-eye";
+let currentIcon;
 
 window.addEventListener('message', (event) => {
   optionsWrapper.innerHTML = '';
 
+  if (!currentIcon || currentIcon !== defaultIcon) {
+    if (currentIcon) {
+      eye.classList.remove(currentIcon);
+    }
+
+    eye.classList.add(defaultIcon);
+    currentIcon = defaultIcon;
+  }
+  
   switch (event.data.event) {
     case 'visible': {
       body.style.visibility = event.data.state ? 'visible' : 'hidden';
-      return (eye.style.fill = 'black');
+      return (eye.style.color = 'black');
     }
 
     case 'leftTarget': {
-      return (eye.style.fill = 'black');
+      return (eye.style.color = 'black');
     }
 
     case 'setTarget': {
-      eye.style.fill = '#cfd2da';
+      if (event.data.icon) {
+        eye.classList.add(event.data.icon);
+        currentIcon = event.data.icon;
+      }
+
+      eye.style.color = '#cfd2da';
 
       if (event.data.options) {
         for (const type in event.data.options) {
