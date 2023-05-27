e84fc59a7b467db8e8e44a5b7786f23a669e1726
iwindd <freewinendlife@gmail.com>
2023-05-28 02:46:20 +0700

    state fix

--- client/state.lua
+++ client/state.lua
@@ -12,7 +12,7 @@ function state.setActive(value)
     isActive = value
 
     if value then
-        SendNuiMessage('{"event": "visible", "state": true')
+        SendNuiMessage('{"event": "visible", "state": true}')
     end
 end
 
