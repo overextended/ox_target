5d7ef1f14d1334d28667b7f179cd590385dbff6b
Crashys <99598990+crashys@users.noreply.github.com>
2023-05-29 10:23:53 +0200

    Portuguese translation
    Portuguese (PT) translation.

---
+++ locales/pt.lua
@@ -0,0 +1,15 @@
+{
+  "toggle_front_driver_door": "Activar porta da frente do condutor",
+  "toggle_front_passenger_door": "Activar porta da frente do passageiro",
+  "toggle_rear_driver_door": "Activar porta de trás do condutor",
+  "toggle_rear_passenger_door": "Activar porta de trás do passageiro",
+  "toggle_hood": "Activar capot",
+  "toggle_trunk": "Activar mala",
+  "debug_box": "(Debug) Caixa",
+  "debug_sphere": "(Debug) Esfera",
+  "debug_police_car": "Carro Policia",
+  "debug_ped": "(Debug) Ped",
+  "debug_vehicle": "(Debug) Veículo",
+  "debug_object": "(Debug) Objecto",
+  "toggle_targeting": "Activar alvo"
+}
