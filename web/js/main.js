2e3d51e2b914af7a0fe81e05a9cf9c45b598ccff
Linden <65407488+thelindat@users.noreply.github.com>
2023-06-16 13:39:46 +1000

    refactor(web): move eye-styling to css

--- web/js/main.js
+++ web/js/main.js
@@ -1,24 +1,24 @@
-import { createOptions } from './createOptions.js';
+import { createOptions } from "./createOptions.js";
 
-const optionsWrapper = document.getElementById('options-wrapper');
+const optionsWrapper = document.getElementById("options-wrapper");
 const body = document.body;
-const eye = document.getElementById('eyeSvg');
+const eye = document.getElementById("eyeSvg");
 
-window.addEventListener('message', (event) => {
-  optionsWrapper.innerHTML = '';
+window.addEventListener("message", (event) => {
+  optionsWrapper.innerHTML = "";
 
   switch (event.data.event) {
-    case 'visible': {
-      body.style.visibility = event.data.state ? 'visible' : 'hidden';
-      return (eye.style.fill = 'black');
+    case "visible": {
+      body.style.visibility = event.data.state ? "visible" : "hidden";
+      return eye.classList.remove("eye-hover");
     }
 
-    case 'leftTarget': {
-      return (eye.style.fill = 'black');
+    case "leftTarget": {
+      return eye.classList.remove("eye-hover");
     }
 
-    case 'setTarget': {
-      eye.style.fill = '#cfd2da';
+    case "setTarget": {
+      eye.classList.add("eye-hover");
 
       if (event.data.options) {
         for (const type in event.data.options) {
