78c361687d5fe527d69bb9746389e877bd9a2628
Linden <65407488+thelindat@users.noreply.github.com>
2023-06-19 13:18:31 +1000

    refactor(web/createOptions): styling and click event tweaks
    Remove fallback style for iconColor when undefined.
    Use a single function to handle click events.

--- web/js/createOptions.js
+++ web/js/createOptions.js
@@ -1,16 +1,24 @@
-import { fetchNui } from './fetchNui.js';
+import { fetchNui } from "./fetchNui.js";
 
-const optionsWrapper = document.getElementById('options-wrapper');
+const optionsWrapper = document.getElementById("options-wrapper");
+
+function onClick() {
+  fetchNui("select", [this.targetType, this.targetId]);
+}
 
 export function createOptions(type, data, id) {
   if (data.hide) return;
 
-  const option = document.createElement('div');
-  option.className = 'option-container';
-  option.innerHTML = `
-      <i class="fa-fw ${data.icon} option-icon" style="color:${data.iconColor || '#cfd2da'}"></i>
-      <p class="option-label">${data.label}</p>
-    `;
-  option.addEventListener('click', () => fetchNui('select', [type, id]));
+  const option = document.createElement("div");
+  const iconElement = `<i class="fa-fw ${data.icon} option-icon" ${
+    data.iconColor ? `style = color:${data.iconColor} !important` : null
+  }"></i>`;
+
+  option.innerHTML = `${iconElement}<p class="option-label">${data.label}</p>`;
+  option.className = "option-container";
+  option.targetType = type;
+  option.targetId = id;
+
+  option.addEventListener("click", onClick);
   optionsWrapper.appendChild(option);
 }
