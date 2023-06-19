d6787234d1210ab40c452f8eedcc3d9bb644cc65
Linden <65407488+thelindat@users.noreply.github.com>
2023-06-19 13:44:28 +1000

    fix(web/createOptions): disable hover effect when clicking option

--- web/js/createOptions.js
+++ web/js/createOptions.js
@@ -3,7 +3,12 @@ import { fetchNui } from "./fetchNui.js";
 const optionsWrapper = document.getElementById("options-wrapper");
 
 function onClick() {
+  // when nuifocus is disabled after a click, the hover event is never released
+  this.style.pointerEvents = "none";
+
   fetchNui("select", [this.targetType, this.targetId]);
+  // is there a better way to handle this? probably
+  setTimeout(() => (this.style.pointerEvents = "auto"), 100);
 }
 
 export function createOptions(type, data, id) {
