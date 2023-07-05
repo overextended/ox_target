9c62fb6cf9f0b3688f09adf435e2c3abc50c068c
Linden <65407488+thelindat@users.noreply.github.com>
2023-07-05 23:33:50 +1000

    refactor(client/compat): use api zone functions

--- client/compat/qb-target.lua
+++ client/compat/qb-target.lua
@@ -130,6 +130,8 @@ local function convert(options)
     return options
 end
 
+local api = require 'client.api'
+
 exportHandler('AddBoxZone', function(name, center, length, width, options, targetoptions)
     local z = center.z
 
@@ -138,15 +140,14 @@ exportHandler('AddBoxZone', function(name, center, length, width, options, targe
         center = vec3(center.x, center.y, z)
     end
 
-    return lib.zones.box({
+    return api.addBoxZone({
         name = name,
         coords = center,
         size = vec3(width, length, (options.useZ or not options.maxZ) and center.z or math.abs(options.maxZ - options.minZ)),
         debug = options.debugPoly,
         rotation = options.heading,
         options = convert(targetoptions),
-        resource = GetInvokingResource(),
-    }).id
+    })
 end)
 
 exportHandler('AddPolyZone', function(name, points, options, targetoptions)
@@ -158,41 +159,27 @@ exportHandler('AddPolyZone', function(name, points, options, targetoptions)
         newPoints[i] = vec3(point.x, point.y, options.maxZ - (thickness / 2))
     end
 
-    return lib.zones.poly({
+    return api.addPolyZone({
         name = name,
         points = newPoints,
         thickness = thickness,
         debug = options.debugPoly,
         options = convert(targetoptions),
-        resource = GetInvokingResource(),
-    }).id
+    })
 end)
 
 exportHandler('AddCircleZone', function(name, center, radius, options, targetoptions)
-    return lib.zones.sphere({
+    return api.addSphereZone({
         name = name,
         coords = center,
         radius = radius,
         debug = options.debugPoly,
         options = convert(targetoptions),
-        resource = GetInvokingResource(),
-    }).id
+    })
 end)
 
 exportHandler('RemoveZone', function(id)
-    if Zones then
-        if type(id) == 'string' then
-            for _, v in pairs(Zones) do
-                if v.name == id then
-                    v:remove()
-                end
-            end
-        end
-
-        if Zones[id] then
-            Zones[id]:remove()
-        end
-    end
+    api.removeZone(id)
 end)
 
 exportHandler('AddTargetBone', function(bones, options)
@@ -205,8 +192,6 @@ exportHandler('AddTargetBone', function(bones, options)
 
     exports.ox_target:addGlobalVehicle(options)
 end)
-
-local api = require 'client.api'
 
 exportHandler('AddTargetEntity', function(entities, options)
     if type(entities) ~= 'table' then entities = { entities } end
--- client/compat/qtarget.lua
+++ client/compat/qtarget.lua
@@ -52,6 +52,8 @@ local function convert(options)
     return options
 end
 
+local api = require 'client.api'
+
 exportHandler('AddBoxZone', function(name, center, length, width, options, targetoptions)
     local z = center.z
 
@@ -60,15 +62,14 @@ exportHandler('AddBoxZone', function(name, center, length, width, options, targe
         center = vec3(center.x, center.y, z)
     end
 
-    return lib.zones.box({
+    return api.addBoxZone({
         name = name,
         coords = center,
         size = vec3(width, length, (options.useZ or not options.maxZ) and center.z or math.abs(options.maxZ - options.minZ)),
         debug = options.debugPoly,
         rotation = options.heading,
         options = convert(targetoptions),
-        resource = GetInvokingResource(),
-    }).id
+    })
 end)
 
 exportHandler('AddPolyZone', function(name, points, options, targetoptions)
@@ -80,41 +81,28 @@ exportHandler('AddPolyZone', function(name, points, options, targetoptions)
         newPoints[i] = vec3(point.x, point.y, options.maxZ - (thickness / 2))
     end
 
-    return lib.zones.poly({
+    return api.addPolyZone({
         name = name,
         points = newPoints,
         thickness = thickness,
         debug = options.debugPoly,
         options = convert(targetoptions),
-        resource = GetInvokingResource(),
-    }).id
+    })
 end)
 
 exportHandler('AddCircleZone', function(name, center, radius, options, targetoptions)
-    return lib.zones.sphere({
+    return api.addSphereZone({
         name = name,
         coords = center,
         radius = radius,
         debug = options.debugPoly,
         options = convert(targetoptions),
-        resource = GetInvokingResource(),
-    }).id
+    })
 end)
 
 exportHandler('RemoveZone', function(id)
-    if Zones then
-        if type(id) == 'string' then
-            for _, v in pairs(Zones) do
-                if v.name == id then
-                    v:remove()
-                end
-            end
-        end
-
-        if Zones[id] then
-            Zones[id]:remove()
-        end
-    end
+    print('RemoveZone')
+    api.removeZone(id)
 end)
 
 exportHandler('AddTargetBone', function(bones, options)
@@ -127,8 +115,6 @@ exportHandler('AddTargetBone', function(bones, options)
 
     exports.ox_target:addGlobalVehicle(options)
 end)
-
-local api = require 'client.api'
 
 exportHandler('AddTargetEntity', function(entities, options)
     if type(entities) ~= 'table' then entities = { entities } end
