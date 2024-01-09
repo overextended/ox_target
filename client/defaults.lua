1ab588984ad19595374115cf2601ce0891f68a11
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2024-01-09 21:48:45 +0100

    refactor: repetitive code to functions

--- client/defaults.lua
+++ client/defaults.lua
@@ -5,6 +5,13 @@ local GetEntityBoneIndexByName = GetEntityBoneIndexByName
 local GetEntityBonePosition_2 = GetEntityBonePosition_2
 local GetVehicleDoorLockStatus = GetVehicleDoorLockStatus
 
+local bones = {
+    [0] = 'dside_f',
+    [1] = 'pside_f',
+    [2] = 'dside_r',
+    [3] = 'pside_r'
+}
+
 local function toggleDoor(vehicle, door)
     if GetVehicleDoorLockStatus(vehicle) ~= 2 then
         if GetVehicleDoorAngleRatio(vehicle, door) > 0.0 then
@@ -12,6 +19,36 @@ local function toggleDoor(vehicle, door)
         else
             SetVehicleDoorOpen(vehicle, door, false, false)
         end
+    end
+end
+
+local function canInteractWithDoor(entity, coords, door, boneId)
+    if GetVehicleDoorLockStatus(entity) > 1 then return end
+
+    local boneName = bones[door]
+    if not boneName or boneId then
+        if IsVehicleDoorDamaged(entity, 4) then return end
+        return #(coords - GetEntityBonePosition_2(entity, boneId)) < 0.9
+    end
+
+
+    boneId = GetEntityBoneIndexByName(entity, 'door_' .. boneName)
+
+    if IsVehicleDoorDamaged(entity, 0) then return end
+
+    if boneId ~= -1 then
+        return #(coords - GetEntityBonePosition_2(entity, boneId)) < 0.5 or
+            #(coords - GetEntityBonePosition_2(entity, GetEntityBoneIndexByName(entity, 'seat_' .. boneName))) < 0.72
+    end
+end
+
+local function onSelectDoor(data, door)
+    local entity = data.entity
+    local owner = NetworkGetEntityOwner(entity)
+    if owner == cache.playerId then
+        toggleDoor(entity, door)
+    else
+        TriggerServerEvent('ox_target:toggleEntityDoor', VehToNet(entity), door)
     end
 end
 
@@ -27,25 +64,10 @@ api.addGlobalVehicle({
         label = locale('toggle_front_driver_door'),
         bones = { 'door_dside_f', 'seat_dside_f' },
         canInteract = function(entity, distance, coords, name)
-            if GetVehicleDoorLockStatus(entity) > 1 then return end
-
-            local boneId = GetEntityBoneIndexByName(entity, 'door_dside_f')
-
-            if IsVehicleDoorDamaged(entity, 0) then return end
-
-            if boneId ~= -1 then
-                return #(coords - GetEntityBonePosition_2(entity, boneId)) < 0.5 or
-                    #(coords - GetEntityBonePosition_2(entity, GetEntityBoneIndexByName(entity, 'seat_dside_f'))) < 0.72
-            end
+            return canInteractWithDoor(entity, coords, 0)
         end,
         onSelect = function(data)
-            local entity = data.entity
-            local owner = NetworkGetEntityOwner(entity)
-            if owner == cache.playerId then
-                toggleDoor(entity, 0)
-            else
-                TriggerServerEvent('ox_target:toggleEntityDoor', VehToNet(entity), 0)
-            end
+            onSelectDoor(data, 0)
         end
     },
     {
@@ -54,24 +76,10 @@ api.addGlobalVehicle({
         label = locale('toggle_front_passenger_door'),
         bones = { 'door_pside_f', 'seat_pside_f' },
         canInteract = function(entity, distance, coords, name)
-            if GetVehicleDoorLockStatus(entity) > 1 then return end
-
-            local boneId = GetEntityBoneIndexByName(entity, 'door_pside_f')
-            if IsVehicleDoorDamaged(entity, 1) then return end
-
-            if boneId ~= -1 then
-                return #(coords - GetEntityBonePosition_2(entity, boneId)) < 0.5 or
-                    #(coords - GetEntityBonePosition_2(entity, GetEntityBoneIndexByName(entity, 'seat_pside_f'))) < 0.72
-            end
+            return canInteractWithDoor(entity, coords, 1)
         end,
         onSelect = function(data)
-            local entity = data.entity
-            local owner = NetworkGetEntityOwner(entity)
-            if owner == cache.playerId then
-                toggleDoor(entity, 1)
-            else
-                TriggerServerEvent('ox_target:toggleEntityDoor', VehToNet(entity), 1)
-            end
+            onSelectDoor(data, 1)
         end
     },
     {
@@ -80,24 +88,10 @@ api.addGlobalVehicle({
         label = locale('toggle_rear_driver_door'),
         bones = { 'door_dside_r', 'seat_dside_r' },
         canInteract = function(entity, distance, coords, name)
-            if GetVehicleDoorLockStatus(entity) > 1 then return end
-
-            local boneId = GetEntityBoneIndexByName(entity, 'door_dside_r')
-            if IsVehicleDoorDamaged(entity, 2) then return end
-
-            if boneId ~= -1 then
-                return #(coords - GetEntityBonePosition_2(entity, boneId)) < 0.5 or
-                    #(coords - GetEntityBonePosition_2(entity, GetEntityBoneIndexByName(entity, 'seat_dside_r'))) < 0.72
-            end
+            return canInteractWithDoor(entity, coords, 2)
         end,
         onSelect = function(data)
-            local entity = data.entity
-            local owner = NetworkGetEntityOwner(entity)
-            if owner == cache.playerId then
-                toggleDoor(entity, 2)
-            else
-                TriggerServerEvent('ox_target:toggleEntityDoor', VehToNet(entity), 2)
-            end
+            onSelectDoor(data, 2)
         end
     },
     {
@@ -106,24 +100,10 @@ api.addGlobalVehicle({
         label = locale('toggle_rear_passenger_door'),
         bones = { 'door_pside_r', 'seat_pside_r' },
         canInteract = function(entity, distance, coords, name)
-            if GetVehicleDoorLockStatus(entity) > 1 then return end
-
-            local boneId = GetEntityBoneIndexByName(entity, 'door_pside_r')
-            if IsVehicleDoorDamaged(entity, 3) then return end
-
-            if boneId ~= -1 then
-                return #(coords - GetEntityBonePosition_2(entity, boneId)) < 0.5 or
-                    #(coords - GetEntityBonePosition_2(entity, GetEntityBoneIndexByName(entity, 'seat_pside_r'))) < 0.72
-            end
+            return canInteractWithDoor(entity, coords, 3)
         end,
         onSelect = function(data)
-            local entity = data.entity
-            local owner = NetworkGetEntityOwner(entity)
-            if owner == cache.playerId then
-                toggleDoor(entity, 3)
-            else
-                TriggerServerEvent('ox_target:toggleEntityDoor', VehToNet(entity), 3)
-            end
+            onSelectDoor(data, 3)
         end
     },
     {
@@ -132,18 +112,10 @@ api.addGlobalVehicle({
         label = locale('toggle_hood'),
         bones = 'bonnet',
         canInteract = function(entity, distance, coords, name, boneId)
-            if GetVehicleDoorLockStatus(entity) > 1 then return end
-            if IsVehicleDoorDamaged(entity, 4) then return end
-            return #(coords - GetEntityBonePosition_2(entity, boneId)) < 0.9
+            return canInteractWithDoor(entity, coords, 4, boneId)
         end,
         onSelect = function(data)
-            local entity = data.entity
-            local owner = NetworkGetEntityOwner(entity)
-            if owner == cache.playerId then
-                toggleDoor(entity, 4)
-            else
-                TriggerServerEvent('ox_target:toggleEntityDoor', VehToNet(entity), 4)
-            end
+            onSelectDoor(data, 4)
         end
     },
     {
@@ -152,18 +124,10 @@ api.addGlobalVehicle({
         label = locale('toggle_trunk'),
         bones = 'boot',
         canInteract = function(entity, distance, coords, name, boneId)
-            if GetVehicleDoorLockStatus(entity) > 1 then return end
-            if IsVehicleDoorDamaged(entity, 5) then return end
-            return #(coords - GetEntityBonePosition_2(entity, boneId)) < 0.9
+            return canInteractWithDoor(entity, coords, 4, boneId)
         end,
         onSelect = function(data)
-            local entity = data.entity
-            local owner = NetworkGetEntityOwner(entity)
-            if owner == cache.playerId then
-                toggleDoor(entity, 5)
-            else
-                TriggerServerEvent('ox_target:toggleEntityDoor', VehToNet(entity), 5)
-            end
+            onSelectDoor(data, 5)
         end
     }
 })
