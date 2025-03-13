92cd4e31cabe8eb09ae945b43e21bd0c12c49287
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-13 08:50:46 +0000

    chore: bump manifest version to v1.17.2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@ game 'gta5'
 -- Resource Information
 name 'ox_target'
 author 'Overextended'
-version '1.17.1'
+version '1.17.2'
 repository 'https://github.com/overextended/ox_target'
 description ''
 
