cc9a926cc00a746141a0a7864d1c5fa2cc2410a4
someone <98275381+someone005@users.noreply.github.com>
2025-03-30 21:19:34 +0200

    ref(client): remove unused functions

--- client/utils.lua
+++ client/utils.lua
@@ -1,33 +1,5 @@
 local utils = {}
 
-local GetWorldCoordFromScreenCoord = GetWorldCoordFromScreenCoord
-local StartShapeTestLosProbe = StartShapeTestLosProbe
-local GetShapeTestResultIncludingMaterial = GetShapeTestResultIncludingMaterial
-
----@param flag number
----@return boolean hit
----@return number entityHit
----@return vector3 endCoords
----@return vector3 surfaceNormal
----@return number materialHash
-function utils.raycastFromCamera(flag)
-    local coords, normal = GetWorldCoordFromScreenCoord(0.5, 0.5)
-    local destination = coords + normal * 10
-    local handle = StartShapeTestLosProbe(coords.x, coords.y, coords.z, destination.x, destination.y, destination.z,
-        flag, cache.ped, 4)
-
-    while true do
-        Wait(0)
-        local retval, hit, endCoords, surfaceNormal, materialHash, entityHit = GetShapeTestResultIncludingMaterial(
-        handle)
-
-        if retval ~= 1 then
-            ---@diagnostic disable-next-line: return-type-mismatch
-            return hit, entityHit, endCoords, surfaceNormal, materialHash
-        end
-    end
-end
-
 function utils.getTexture()
     return lib.requestStreamedTextureDict('shared'), 'emptydot_32'
 end
